eb289c3f55fc02265bd6693f1367baa25f2717d4
jbubl <158827580+jbubl@users.noreply.github.com>
2026-08-30 21:21:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(7860, () => console.log('Web server active on port 7860'));
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+const GUILD_ID = "YOUR_GUILD_ID";
+const CHANNEL_ID = "YOUR_CHANNEL_ID";
+
+client.once('ready', async () => {
+    try {
+        console.log(`Logged in as ${client.user.tag}`);
+        const guild = await client.guilds.fetch(GUILD_ID);
+        const channel = await guild.channels.fetch(CHANNEL_ID);
+        
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: guild.id,
+            adapterCreator: guild.voiceAdapterCreator,
+            selfDeaf: false,
+            selfMute: true
+        });
+        console.log(`Successfully connected to voice channel: ${channel.name}`);
+    } catch (error) {
+        console.error("Failed to connect to voice channel:", error);
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
